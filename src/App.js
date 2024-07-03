2364ef543486715f8c695aec1a275e83aad6b36b
vipin <mainvipin@gmail.com>
2024-07-03 13:07:13 +0530

    First app change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To Mealigo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Download Mealigo App
         </a>
       </header>
     </div>
