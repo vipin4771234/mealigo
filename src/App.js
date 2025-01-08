9ded6830fa07ae7f411d1d77759981697f7e8c62
vipin <mainvipin@gmail.com>
2025-01-08 15:26:07 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Blog from './screens/Blog/Blog';
+import About from './screens/About/About';
+import Contact from './screens/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <p style={{color:'black'}}>
-          Welcome To Mealigo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Download Mealigo App
-        </a>
-      </header>
+      <Routes>
+          <Route path="/" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/invoiceUpload" element={<ProtectedRoute><Home /></ProtectedRoute>} /> */}
+          <Route path="*" element={<Blog />} />
+        </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,27 +5,65 @@ import about from "../../assets/images/About.png";
 import blog from "../../assets/images/blog.png";
 import categories from "../../assets/images/categories.png";
 import contact from "../../assets/images/call.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation();
   return (
     <div className="header-container">
-      <div className="header-item-container">
-        <img src={blog} className="header-icons" alt="mealigo-logo" />
-        <a className="header-item-text" href="https://www.mealigo.com">Blogs</a>
-      </div>
+      <Link
+        to={"/"}
+        className={`header-item-text ${
+          location.pathname === "/" ? "selected" : ""
+        }`}
+      >
+        <div className="header-item-container">
+          <img src={blog} className="header-icons" alt="mealigo-logo" />
+          <a className="header-item-text" href="https://www.mealigo.com">
+            Blogs
+          </a>
+        </div>
+      </Link>
+      <Link
+        to={"/"}
+        className={`header-item-text ${
+          location.pathname === "/" ? "selected" : ""
+        }`}
+      >
       <div className="header-item-container">
         <img src={categories} className="header-icons" alt="mealigo-logo" />
-        <a className="header-item-text" href="https://www.mealigo.com">Category</a>
+        <a className="header-item-text" href="https://www.mealigo.com">
+          Category
+        </a>
       </div>
+      </Link>
       <img src={headerlogo} className="header-logo" alt="mealigo-logo" />
+      <Link
+        to={"/about"}
+        className={`header-item-text ${
+          location.pathname === "/" ? "selected" : ""
+        }`}
+      >
       <div className="header-item-container">
         <img src={about} className="header-icons" alt="mealigo-logo" />
-        <a className="header-item-text" href="https://www.mealigo.com">About me</a>
+        <a className="header-item-text" href="https://www.mealigo.com">
+          About me
+        </a>
       </div>
+      </Link>
+      <Link
+        to={"/contact"}
+        className={`header-item-text ${
+          location.pathname === "/" ? "selected" : ""
+        }`}
+      >
       <div className="header-item-container">
         <img src={contact} className="header-icons" alt="mealigo-logo" />
-        <a className="header-item-text" href="https://www.mealigo.com">Contact</a>
+        <a className="header-item-text" href="https://www.mealigo.com">
+          Contact
+        </a>
       </div>
+      </Link>
     </div>
   );
 };
