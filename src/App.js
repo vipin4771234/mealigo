39539c68c0090aaf3192f26836ec5ff5251fb631
vipin <mainvipin@gmail.com>
2025-01-08 15:26:35 +0530

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Blog from './screens/Blog/Blog';
 import About from './screens/About/About';
 import Contact from './screens/Contact/Contact';
